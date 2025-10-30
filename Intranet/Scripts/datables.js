620ca2d05625e72d26f9b8281754cdb37c748478
Arthur <Sistemas1@softhair.com.br>
2025-10-30 08:59:48 -0300

    subindo js em scripts

--- Intranet/Scripts/datables.js
+++ Intranet/Scripts/datables.js
@@ -1,11 +1,12 @@
-// TABELA DE ANIVERSARIANTES
-DataTable.datetime('DD/MM/YYYY');
-new DataTable('#niver', {
-    order: [1, 'asc'],
+new DataTable('#example', {
     searching: false,
     info: false,
     paging: false,
-
+    order: [1,'asc'],
+    language: {
+        url: "https://cdn.datatables.net/plug-ins/2.3.4/i18n/pt-BR.json"
+    },
+   /* line-rocha*/
     createdRow: function (row, data, dataIndex) {
         let hoje = new Date();
         let dia = hoje.getDate();
@@ -15,64 +16,34 @@ new DataTable('#niver', {
         let dataAniver = $('td', row).eq(1).text().trim();
         let [diaNiver, mesNiver] = dataAniver.split('/').map(Number);
 
-        // adiciona cor especial pra quem faz aniversário hoje
         if (dia === diaNiver && mes === mesNiver) {
+            // adiciona classe na <tr>
             $(row).addClass('linha-aniver');
         }
     }
 });
-
-$(document).ready(function () {
-    let hoje = new Date();
-    let dia = hoje.getDate();
-    let mes = hoje.getMonth() + 1;
-
-    $('#niver tbody tr').each(function () {
-        let dataAniver = $('td', this).eq(1).text().trim();
-        let [diaNiver, mesNiver] = dataAniver.split('/').map(Number);
-
-        if (mesNiver === mes && diaNiver >= dia) {
-            $(this).show();
-        } else {
-            $(this).hide();
-        }
-    });
-});
-
-new DataTable('#hours', {
+new DataTable('hours', {
     searching: false,
+    language: {
+        url: "https://cdn.datatables.net/plug-ins/2.3.4/i18n/pt-BR.json"
+    }
 });
-
-DataTable.datetime('DD/MM/YYYY');
 new DataTable('#gym', {
-    order: [0, 'asc'],
-    paging: false,
-    info: false,
-    searching: false,
-});
-
-$(document).ready(function () {
-    let hoje = new Date();
-    let dia = hoje.getDate();
-    let mes = hoje.getMonth() + 1;
-
-    $('#gym tbody tr').each(function () {
-        let dataGym = $('td', this).eq(0).text().trim();
-        let [diaGym, mesGym] = dataGym.split('/').map(Number);
-
-        if (mesGym === mes && diaGym >= dia) {
-            $(this).show();
-        } else {
-            $(this).hide();
-        }
-    });
-});
-
-new DataTable('#ramal', {
     paging: false,
     info: false,
     scrollY: '800px',
+    searching: false,
+    paging: false,
     language: {
-        url: "https://cdn.datatables.net/plug-ins/2.3.4/i18n/pt-BR.json",
+        url: "https://cdn.datatables.net/plug-ins/2.3.4/i18n/pt-BR.json"
+    }
+});
+new DataTable('#ram', {
+    paging: false,
+    info: false,
+    scrollY: '800px',
+    paging: false,
+    language: {
+        url: "https://cdn.datatables.net/plug-ins/2.3.4/i18n/pt-BR.json"
     }
 });
